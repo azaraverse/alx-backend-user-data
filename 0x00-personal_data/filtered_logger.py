773e09a0ad0c3bbeff816e95eedec22847275ebb
Azara <yyushahu02@gmail.com>
2024-08-01 16:57:15 +0000

    implement the log formatter to custom format a log record

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -20,3 +20,27 @@ def filter_datum(
         regex = fr'(?<={field}=)[^{separator}]*'
         message = re.sub(regex, redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filters values in incoming log records using filter_datum()
+
+        Args:
+            record: information or event being logged.
+        """
+        return filter_datum(
+            self.fields, self.REDACTION, super().format(record),
+            self.SEPARATOR
+        )

dc74b69f83c6b643108ad2aaca16dfdd50161675
Azara <yyushahu02@gmail.com>
2024-08-02 02:41:29 +0000

    successfully validate a password matching a hashed version

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,9 +7,23 @@ def hash_password(password: str) -> bytes:
     """ A function that hashes and encrypts a password
 
     Args:
-        password: A string representing user password
+        password: a string representing user password
     Returns:
         A byte string
     """
     pwd = password.encode("utf-8")
     return bcrypt.hashpw(pwd, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ A function that validates that a password matches a
+    corresponding hashed password
+
+    Args:
+        hashed_password: encrypted password in bytes
+        password: password string literal
+    Returns:
+        Boolean
+    """
+    pwd = password.encode("utf-8")
+    return bcrypt.checkpw(pwd, hashed_password)

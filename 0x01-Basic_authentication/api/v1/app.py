5bd2d84b70bd25a878d7d8a4b39d761f1361bf85
Azara <yyushahu02@gmail.com>
2024-08-07 14:33:14 +0000

    update return type annotations

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,7 +2,7 @@
 """
 Route module for the API
 """
-from os import getenv
+from typing import Tuple, Dict
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -19,21 +19,21 @@ if os.getenv('AUTH_TYPE') == auth:
 
 
 @app.errorhandler(404)
-def not_found(error) -> str:
+def not_found(error) -> Tuple[Dict[str, str], int]:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
+def unauthorized(error) -> Tuple[Dict[str, str], int]:
     """ Unauthorized error handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> str:
+def forbidden(error) -> Tuple[Dict[str, str], int]:
     """ Forbidden error handler
     """
     return jsonify({"error": "Forbidden"}), 403
@@ -57,6 +57,6 @@ def before_request():
 
 
 if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
+    host = os.getenv("API_HOST", "0.0.0.0")
+    port = os.getenv("API_PORT", "5000")
     app.run(host=host, port=port)

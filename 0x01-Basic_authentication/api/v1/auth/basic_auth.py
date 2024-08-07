99b9111aa5528b57b13377db6bb586cb4464574c
Azara <yyushahu02@gmail.com>
2024-08-07 15:27:43 +0000

    add extraction method on base64 auth_header to return value after Basic keyword

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,3 +6,14 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Basic Authenticator class that inherits from Auth()
     """
+    def extract_base64_authorization_header(
+            self, authorization_header: str
+    ) -> str:
+        """ Extracts base64 authorization header and returns the value
+        after the Basic keyword
+        """
+        if authorization_header is None or type(authorization_header) != str:  # nopep8
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.split(' ')[1]

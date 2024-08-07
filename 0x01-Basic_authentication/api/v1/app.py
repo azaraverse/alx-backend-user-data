63dca05d40752cdd88607c816688c3fc9b33cf4b
Azara <yyushahu02@gmail.com>
2024-08-07 14:19:11 +0000

    remove typecasting of int

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -58,5 +58,5 @@ def before_request():
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
-    port = int(getenv("API_PORT", "5000"))
+    port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)

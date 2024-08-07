e7c062a6abe5fb13f3b6b1bb2eea7d68f047fb1a
Azara <yyushahu02@gmail.com>
2024-08-07 17:44:29 +0000

    implement user object extraction from database using user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """ Basic Auth class """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -47,3 +49,33 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(':'))
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str
+    ) -> TypeVar('User'):
+        """ Extracts user obj from given credentials and returns the
+        User object
+        """
+        if user_email is None or type(user_email) != str:
+            return None
+        if user_pwd is None or type(user_pwd) != str:
+            return None
+        try:
+            users = User.search({"email": user_email})
+        except Exception:
+            return None
+        if not users:
+            return None
+
+        user = users[0]
+        if not User.is_valid_password(user, user_pwd):
+            return None
+        return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """"""
+        user = self.authorization_header(request)
+        if not user:
+            return None
+
+        self.extract_base64_authorization_header(request.headers)

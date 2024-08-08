8d2270a2b83a89200a42cb05ef865b8d5b4860ad
Azara <yyushahu02@gmail.com>
2024-08-08 13:46:14 +0000

    update the before_request decorator with extended excluded paths for require path method and abort if session_cookie returns none in the before_request handler

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -53,13 +53,16 @@ def before_request():
     """ Executed before each request is made
     """
     paths = [
-        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
+        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
     if auth is None:
         return
     if not auth.require_auth(request.path, paths):
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and auth.session_cookie(
+        request
+    ) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)

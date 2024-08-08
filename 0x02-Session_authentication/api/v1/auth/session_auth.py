a2a98ba9a16d02abc063b1b6933b31d966f54f7f
Azara <yyushahu02@gmail.com>
2024-08-08 13:15:14 +0000

    implement a method that generates a session id for a given user id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 """ Session Auth Class """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ Session Authentication class
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a session id for a given user id
+        """
+        if user_id is None or type(user_id) != str:
+            return None
+        self.session_id = str(uuid4())
+        self.user_id_by_session_id[self.session_id] = user_id
+        return self.session_id
